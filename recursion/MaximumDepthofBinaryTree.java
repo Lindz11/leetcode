a81c3fedcb43147db6c1c37e126da07407b6e9fc
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-01-24 23:00:54 -0500

    Update MaximumDepthofBinaryTree.java

--- recursion/MaximumDepthofBinaryTree.java
+++ recursion/MaximumDepthofBinaryTree.java
@@ -1,20 +1,13 @@
 class Solution {
     public int maxDepth(TreeNode root) {
-        int left = 0; 
-        int right = 0; 
-        if(root == null)
+        // If root is null return 0
+        if( root == null)
             return 0; 
-        if(root.left == null && root.right == null) 
-            return 1; 
-        TreeNode temp = root;
-        if(temp.left!= null)
-            left = 1 + maxDepth(temp.left); 
-        if(temp.right!= null)
-            right = 1 + maxDepth(temp.right); 
+        // Do a depth first search traversing left and right 
+       int left = 1 + maxDepth(root.left); 
+       int right = 1 + maxDepth(root.right); 
         
-        if(left > right)
-            return left; 
-        else
-            return right; 
+        return Math.max(left,right);  
+        
     }
 }
