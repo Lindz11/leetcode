9dde2212ef049a6217e6e55a6c4672fb528f4b14
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-02-03 02:09:32 -0500

    Create DefanginganIPAddress.java

---
+++ string/DefanginganIPAddress.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder(); 
+        for(char ch: address.toCharArray()) {
+            if(ch == '.'){
+                sb.append("[.]"); 
+                continue; 
+            }
+            sb.append(ch); 
+        }
+        
+        return sb.toString(); 
+    }
+}
