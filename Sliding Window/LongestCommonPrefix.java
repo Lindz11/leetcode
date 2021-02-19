732a8a81ca965103a329aa01f7c4bc89563de2db
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-02-19 00:05:29 -0500

    Update LongestCommonPrefix.java

--- Sliding Window/LongestCommonPrefix.java
+++ Sliding Window/LongestCommonPrefix.java
@@ -1,24 +1,20 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String common = ""; 
-        if(strs == null || strs.length == 0)
+        if(strs.length == 0 || strs == null)
             return common; 
-        int length = strs.length; 
-        int index = 0;  
-        // Take the first word from the array 
-        char [] first = strs[0].toCharArray();
-        for(int j = 0; j < first.length; j++){
-            // Compare each letter one at a time 
-            char c = first[j];
-            for(int i = 1; i < strs.length; i++) {
-                // Check if we go out of bounds or if the letters do not match
-                if(index >=strs[i].length() || c != strs[i].charAt(index))
+        String word = strs[0]; 
+        int index = 0; 
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i); 
+            for(int j = 1; j < strs.length; j++) {
+                if(index >= strs[j].length() || c != strs[j].charAt(index))
+                {
                     return common; 
+                } 
             }
-            // increment both the common prefix and the index
             common+=c; 
-            index++;
-            
+            index++;  
         }
         
         return common; 
