c1a553bf971cf58470bffab580f7d4a0fee86ff1
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-03-15 22:55:29 -0400

    Create EmployeesWithBonuses.java

---
+++ SQL Questions/EmployeesWithBonuses.java
@@ -0,0 +1,8 @@
+-- Find employees whose bonus is less than $150.
+-- Output the first name along with the corresponding bonus.
+select 
+    first_name,
+    bonus
+from employee
+where bonus < 150
+order by first_name asc 
