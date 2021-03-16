b9bc8845c54211b384587aacc33f1e9a21efeba7
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-03-15 22:51:59 -0400

    Create NotRefferedEmployess.sql

---
+++ SQL Questions/NotRefferedEmployess.sql
@@ -0,0 +1,7 @@
+-- Find employees that are not referred by the manager id 1.
+-- Output the first name of the employee.
+select 
+    first_name
+from employee
+where manager_id != 1
+order by first_name asc
