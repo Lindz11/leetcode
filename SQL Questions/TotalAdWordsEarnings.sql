a044ad0d23e2c373a62370495dc4207207f2da4d
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-03-16 22:41:14 -0400

    Create TotalAdWordsEarnings.sql

---
+++ SQL Questions/TotalAdWordsEarnings.sql
@@ -0,0 +1,7 @@
+-- Find the total AdWords earnings for each business type. Output the business types along with the total
+-- earnings.
+select 
+    business_type,
+    sum(adwords_earnings) as total_earnings 
+from google_adwords_earnings
+group by business_type
