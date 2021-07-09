a3a4e269b3e6adee2e9062b00a8d4c144f5ff98a
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-07-08 22:37:04 -0400

    Create FindIndustriesWithHighestNumberOfCompanies.sql

---
+++ SQL Questions/FindIndustriesWithHighestNumberOfCompanies.sql
@@ -0,0 +1,7 @@
+-- Find industries with the highest number of companies. Output the industry along with the number of companies. Sort records based on the number of companies in descending order.
+select 
+    industry,
+    count(company) as n_companies
+from forbes_global_2010_2014
+group by industry
+order by n_companies desc
