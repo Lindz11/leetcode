bb6d784f5c58fe16a1328da4b0391602c6876369
Lindz11 <lindsaygriffith@knights.ucf.edu>
2021-10-26 21:08:08 -0400

    Create ApartmentsInNewYorkCity.sql

---
+++ SQL Questions/ApartmentsInNewYorkCity.sql
@@ -0,0 +1,5 @@
+-- Apartments in New York City and Harlem -- Find the search details of 50 apartment searches the Harlem neighborhood of New York City.
+select * 
+from airbnb_search_details
+where city = 'NYC' and neighbourhood ='Harlem'and property_type ='Apartment'
+Limit 50
